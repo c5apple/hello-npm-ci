7121626949cc29cf244c5cb5e5b7b98ce1f14f91
c5apple <c5apple_party@yahoo.co.jp>
2025-02-23 01:19:07 +0900

    http-serverを試す

--- cli.js
+++ cli.js
@@ -1,3 +1,16 @@
 #!/usr/bin/env node
 
-console.log("Hello from your CLI tool!");
+const { exec } = require('child_process');
+
+// コマンドを実行
+exec('npx http-server', (error, stdout, stderr) => {
+  if (error) {
+    console.error(`Error: ${error.message}`);
+    return;
+  }
+  if (stderr) {
+    console.error(`Stderr: ${stderr}`);
+    return;
+  }
+  console.log(`Output: ${stdout}`);
+});
